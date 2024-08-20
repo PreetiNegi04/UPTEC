5c53cf449d3dad2b992bf532f3d7eaf4452eceae
Preeti <negipreeti2003@gmail.com>
2024-08-20 17:10:25 +0530

    added validation

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify, render_template, url_for, redirect
 from flask_pymongo import PyMongo
+import re
+import bcrypt
 
 app = Flask(__name__)
 
 app.config['MONGO_URI'] = "mongodb://localhost:27017/mydatabase"
 mongo = PyMongo(app).db
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('auth-login.html')
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -27,13 +29,20 @@ def register():
             message = 'Username already exists!'
         elif email_found:
             message = 'Email already exists!'
+        elif validate_email(email) == False:
+            message = 'Wrong email!'
+        elif validate_username(username) == False:
+            message = 'Username must be at least 8 characters long and should contain atleast 1 uppercase, 1 lowercase and 1 digit!'
+        elif validate_password(password) == False:
+            message = 'Password must be at least 8 characters long and should contain atleast 1 uppercase, 1 lowercase and 1 digit!'
         elif password != confirm_password:
             message = 'Passwords do not match!'
         else:
+            hashed_password = hash_password(password)
             user = {
                 'username': username,
                 'email': email,
-                'password': password
+                'password': hashed_password
             }
             mongo.user.insert_one(user)
 
@@ -54,8 +63,8 @@ def login():
         user_found = mongo.user.find_one({'username': username})
         
 
-        if user_found and user_found['password'] == password:
-            return render_template('index.html', username = uname)
+        if user_found and check_password(user_found['password'], password):
+            return render_template('index.html', username = username)
         else:
             message = 'Invalid username or password!'
 
@@ -80,12 +89,42 @@ def forget_password():
             if password != confirm_password:
                 message = 'Passwords do not match!'
             else:
-                mongo.user.update_one({'email': email}, {'$set': {'password': password}})
+                hashed_password = hash_password(password)
+                mongo.user.update_one({'email': email}, {'$set': {'password': hashed_password}})
                 message = 'Password updated successfully!'
                 return redirect(url_for('login'), message = message)
         else:
             message = 'Email doesn\'t exist!'
     return render_template('auth-forgot-password.html', message = message)
+
+def validate_username(username):
+    # Username should be at least 8 characters long, containing at least one uppercase letter, one lowercase letter, and one digit
+    if re.fullmatch(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$', username):
+        return True
+    return False
+
+def validate_password(password):
+    # Password should be at least 8 characters long, containing at least one uppercase letter, one lowercase letter, and one digit
+    if re.fullmatch(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@#$%^&+=]{8,}$', password):
+        return True
+    return False
+
+def validate_email(email):
+    # Email should be in a valid format
+    if re.fullmatch(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
+        return True
+    return False
+
+def hash_password(password):
+    # Generate a salt
+    salt = bcrypt.gensalt()
+    # Hash the password with the salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+def check_password(hashed_password, user_password):
+    # Check if the provided password matches the hashed password
+    return bcrypt.checkpw(user_password.encode('utf-8'), hashed_password)
 
 if __name__ == '__main__':
     app.run(debug=True)
